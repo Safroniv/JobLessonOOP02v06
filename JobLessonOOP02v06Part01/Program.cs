2851cd025f1300ad6ccc3eaa99c9dfa949fd8046
Safroniv <safroniv@mail.ru>
2022-04-03 19:17:32 +0300

    Добавьте файлы проекта.

---
+++ JobLessonOOP02v06Part01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobLessonOOP02v06Part01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
